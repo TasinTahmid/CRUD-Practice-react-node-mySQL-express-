49902623dcd4fd4f654031ca9a0680c28c65b8c1
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-02-20 20:32:40 +0600

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from "react";
+import Axios from 'axios';
 
 function App() {
   const [name, setName] = useState('');
@@ -8,8 +9,14 @@ function App() {
   const [position, setPosition] = useState('');
   const [wage, setWage] = useState(0);
 
-  const displayInfo = () => {
-    console.log(name, age, country, position, wage);
+  const addEmployee = () => {
+    Axios.post("http://localhost:5000/api/create",{
+      name,
+      age,
+      country,
+      position,
+      wage
+    }).then(() => {console.log("success..")});
   }
 
   return (
@@ -19,15 +26,20 @@ function App() {
       <div className="information">
         <label htmlFor="name">Name:</label>
         <input type="text" onChange={(e)=>{setName(e.target.value)}} />
+
         <label htmlFor="age">Age:</label>
         <input type="number" onChange={(e)=>{setAge(e.target.value)}}/>
+
         <label htmlFor="courntry">Country:</label>
         <input type="text" onChange={(e)=>{setCountry(e.target.value)}}/>
+
         <label htmlFor="position">Position:</label>
         <input type="text" onChange={(e)=>{setPosition(e.target.value)}}/>
+
         <label htmlFor="wage">Wage (per year):</label>
         <input type="text" onChange={(e)=>{setWage(e.target.value)}}/>
-        <button onClick = {displayInfo}>Add Employee</button>
+
+        <button onClick = {addEmployee}>Add Employee</button>
       </div>
 
     </div>
--- server/server.js
+++ server/server.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "employee_management_system",
+});
 
 app.get('/', (req, res) =>{
+  
   res.send('This is get request from get(/)');
 });
 
+app.post('/api/create',(req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  console.log(name, age, wage);
+
+  db.query(
+    'INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)', 
+  [name, age, country, position, wage], 
+  (err, result) => {
+    if(err) {return console.log(err);}
+
+    res.send("Values Inserted.");
+  });
+});
 
 app.listen(5000, () => {
   console.log('Listening to the port 5000...');
